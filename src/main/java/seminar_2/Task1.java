9ff9cb688533c5d09290333e4d1c6e53386d081e
BorAlex <alexander44@bk.ru>
2023-08-27 15:17:58 +0300

    add Task1

--- src/main/java/seminar_2/Task1.java
+++ src/main/java/seminar_2/Task1.java
@@ -1,7 +1,25 @@
 package seminar_2;
 
+import java.util.Scanner;
+
 public class Task1 {
     public static void main(String[] args) {
-        
+         enterFractionalNumber();
+
+    }
+
+    public static void enterFractionalNumber(){
+        Scanner scanner = new Scanner(System.in);
+        while (true){
+            try{
+                System.out.println("Введите любое число: ");
+                String number = scanner.nextLine();
+                float float_num = Float.parseFloat(number);
+                System.out.println("Введенное число - " + float_num);
+                break;
+            } catch (NumberFormatException e){
+                System.out.println("Введено не число!!!! Повторите ввод!!!");
+            }
+        }
     }
 }
